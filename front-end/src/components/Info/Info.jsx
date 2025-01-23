9b53009502030fb04c9df141772b1ec592684435
srdr93 <srdryldz93@gmail.com>
2025-01-23 22:23:31 +0300

    Header is done.

---
+++ front-end/src/components/Info/Info.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./info.css";
+
+const Info = () => {
+  return <div>Info</div>;
+};
+
+export default Info;
---
+++ front-end/src/components/Topics/Topics.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./topics.css";
+
+const Topics = () => {
+  return <div>Topics</div>;
+};
+
+export default Topics;
