4e33e92406625ebd9acff7978f9d6447b3d09073
srdr93 <srdryldz93@gmail.com>
2025-01-27 14:32:40 +0300

    Front-end back-end and responsive design are done. site is deployed.

--- front-end/src/components/Blog/Blog.jsx
+++ front-end/src/components/Blog/Blog.jsx
@@ -1,8 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./blog.css";
+import { client, urlFor } from "../../client";
 
 const Blog = () => {
-  return <div>Blog</div>;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "posts"]';
+
+    client.fetch(query).then((data) => {
+      setPosts(data);
+    });
+  }, []);
+  return (
+    <section id="blog" className="pink">
+      <div className="wrapper">
+        <h2 className="light">Latest Posts</h2>
+        <div className="content-container">
+          {posts.map((post) => (
+            <div className="post">
+              <div className="tag">{post.label}</div>
+              <a href="#">
+                <img src={urlFor(post.thumbnail)} alt="" />
+              </a>
+
+              <a href="#">
+                <h3 className="post-title">{post.title}</h3>
+              </a>
+            </div>
+          ))}
+        </div>
+
+        <div className="btn-container">
+          <a href="#" className="all-posts-btn">
+            All Posts
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Blog;
--- front-end/src/components/Navbar/Navbar.jsx
+++ front-end/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.css";
+import { menuIcon, closeIcon } from "../../assets";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [isActive, setIsActive] = useState(false);
+  return (
+    <>
+      <nav>
+        <a href="#" className="logo">
+          Sound<span className="red">Dzign</span>
+        </a>
+        <ul>
+          <li>
+            <a href="#topics">Course Details</a>
+          </li>
+          <li>
+            <a href="#info">About</a>
+          </li>
+          <li>
+            <a href="#blog">Blog</a>
+          </li>
+          <li>
+            <a href="#testimonials">Testimonials</a>
+          </li>
+        </ul>
+
+        <div onClick={() => setIsActive(true)} className="menu-icon">
+          <img src={menuIcon} alt="" />
+        </div>
+      </nav>
+
+      <div className={`mobile-menu-container ${isActive ? "active" : ""}`}>
+        <div onClick={() => setIsActive(false)} className="close-icon">
+          <img src={closeIcon} alt="" />
+        </div>
+
+        <ul className="menu-items">
+          <li>
+            <a href="#topics" onClick={() => setIsActive(false)}>
+              Course Details
+            </a>
+          </li>
+          <li>
+            <a href="#info" onClick={() => setIsActive(false)}>
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#blog" onClick={() => setIsActive(false)}>
+              Blog
+            </a>
+          </li>
+          <li>
+            <a href="#testimonials" onClick={() => setIsActive(false)}>
+              Testimonials
+            </a>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 };
 
 export default Navbar;
